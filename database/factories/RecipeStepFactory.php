df29067848df395f58f0c6d80f5f0e008b2b2c4a
GrzegorczykP <pawlo6697@gmail.com>
2020-06-28 14:33:22 +0200

    Add some models and migrations

---
+++ database/factories/RecipeStepFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\RecipeStep;
+use Faker\Generator as Faker;
+
+$factory->define(RecipeStep::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
